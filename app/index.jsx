89f852f06307aa07f3eb0795eb54e1b45cad32c5
Eylül Dalkıran <74823587+eyluldalkiran@users.noreply.github.com>
2024-10-02 15:51:15 +0300

    addition to last commit

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ fontFamily: "outfit" }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
