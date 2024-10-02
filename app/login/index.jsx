1ba18c28e23cfe0efd96a3bd2800f45c6bfaba60
Eylül Dalkıran <74823587+eyluldalkiran@users.noreply.github.com>
2024-10-02 17:08:12 +0300

    feat(login): added sign in with google sign in functionality using Clerk

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -6,9 +6,41 @@ import {
   SafeAreaView,
   TouchableOpacity,
 } from "react-native";
+import { useOAuth } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+import * as WebBrowser from "expo-web-browser";
 import React from "react";
-
+import { useCallback, useEffect } from "react";
+import { Link } from "expo-router";
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const onPress = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp } = await startOAuthFlow({
+        redirectUrl: Linking.createURL("/home", { scheme: "myapp" }),
+      });
+
+      if (createdSessionId) {
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <SafeAreaView>
       <Image
@@ -20,7 +52,7 @@ export default function LoginScreen() {
         <Text style={styles.title}>to embrace your new friend?</Text>
       </View>
       <View style={{ justifyContent: "center", alignItems: "center" }}>
-        <TouchableOpacity style={styles.button} onPress={() => nav}>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
           <Text style={styles.buttonText}>Get Started</Text>
         </TouchableOpacity>
       </View>
